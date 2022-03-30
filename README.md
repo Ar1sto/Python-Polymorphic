26d1b511da1c69aa0397f09be8e67c08ac8880c4
Ar1sto <56418578+Ar1sto@users.noreply.github.com>
2022-03-30 20:59:32 +0200

    readme file and main-code

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple Python-Polymorphic Code Exampel
+English:
+         This code should simplify the principle behind polymorphism. There are different ways to make code polymorph, but this method is easy to understand is clear and fun. Which code is executed can be linked to            conditions. Maybe you can even improve the code or you can customize it to your needs. I hope I can bring you closer to the principle of polymorphism. - AR1STO
+
+Deutsch:
+         Dieser Code soll vereinfacht das Prinzip hinter Polymorphismus zeigen. Es gibt verschieden wege Code Polymorph zu machen, doch diese Methode ist leicht verständlich ist übersichtlich und macht Spaß. Welcher Code ausgeführt wird lässt sich an Bedingungen anknüpfen. Vielleicht kannst du den Code sogar verbessern oder du kannst es an deine Bedürfnisse anpassen. Ich hoffe ich kann dich dem Prinzip des Polymorphismus näher bringen. - Ar1sto
+
+- Sie sollten folgende Module heruntergeladen haben: (Dies hat nichts mit dem Polymorphistischem-Prinzip zutun)
+   - Base64
+   - String
+- Sie sollten folgende Software heruntergeladen haben: (Dies hat nichts mit dem Polymorphistischem-Prinzip zutun)
+   - LOLCat
+
+## Ausgabe des Script's
+![Ausgabe des scripts beim 1.mal](https://github.com/Ar1sto/Python-Polymorphic/blob/main/__images__/polycode1.png)
+![Ausgabe des scripts beim 2.mal](https://github.com/Ar1sto/Python-Polymorphic/blob/main/__images__/polycode2.png)
+![Ausgabe des scripts beim 3.mal](https://github.com/Ar1sto/Python-Polymorphic/blob/main/__images__/polycode3.png)
+
+Bei Fragen, Kritik oder Anregungen kontaktieren sie: 
+
+       Discord: Omniscius#6583 
---
+++ polymorph.py
@@ -0,0 +1,93 @@
+#/usr/bin/Python3
+#/usr/bin/env Python3
+#Author: Ar1sto (https://github.com/Ar1sto) | Discord: Omniscius#6583
+from random import choice as polyfunction
+polycode = ("one()", "two()", "three()")
+class polymorphiccode():
+
+# Funktion 1: Normaler String
+
+    def one():
+        codeone = """
+from subprocess import run
+print ("Polymorphic-Code Example: 1")
+i = "Omniscius"
+example_one = f'''
+                ╔═════════════════════╗
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ╚═════════════════════╝
+                       {i}
+               '''
+run(["lolcat"], input=example_one, text=True)
+
+                  """
+        exec(codeone)
+        
+# Funktion 2: Hexadezimal zu String
+        
+    def two():
+        codetwo = """
+from subprocess import run
+print ("Polymorphic-Code Example: 2")
+i = "4f6d6e6973636975730a"
+convert = bytes.fromhex(f'{i}').decode('utf-8')
+convert = convert.replace(r'''"\n"''', "")
+example_two = f'''
+                ╔═════════════════════╗
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ╚═════════════════════╝
+                       {convert}
+               '''
+run(["lolcat"], input=example_two, text=True)
+                  """
+        exec(codetwo)
+        
+# Funktion 3: String-Funktion reverse-String zu Base85 und zurück dekodiert dann wieder reverse
+
+    def three():
+        codethree = """
+from subprocess import run
+import string, base64
+print ("Polymorphic-Code Example: 3")
+name = list(string.ascii_lowercase+string.ascii_uppercase)
+i = name[40]+name[12]+name[13]+name[8]+name[18]+name[2]+name[8]+name[20]+name[18]
+i = i[::-1]
+i_bytes = i.encode("ascii")
+i_base_bytes = base64.a85encode(i_bytes)
+i_bcoded = i_base_bytes.decode("ascii")
+i_bcoded_bytes = i_bcoded.encode("ascii")
+i_bcoded_decoder = base64.a85decode(i_bcoded_bytes)
+i_decoded = i_bcoded_decoder.decode("ascii")
+i_reverse_decoded = i_decoded[::-1]
+example_three = f'''
+                ╔═════════════════════╗
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ║█████████████████████║
+                ╚═════════════════════╝
+                       {i_reverse_decoded}
+               '''
+run(["lolcat"], input=example_three, text=True)
+                  """
+        exec(codethree)
+execution = polyfunction(polycode)
+exec(f"polymorphiccode.{execution}")
